0dbfd6351f5e426c8c86d6da7515260ee43a803c
Jesús Calero Jerónimo <>
2019-11-19 21:03:57 +0000

    Principal

---
+++ Prueba001Pral/src/prueba001pral/Prueba001Pral.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba001pral;
+
+/**
+ *
+ * @author Jesús Calero Jerónimo
+ */
+public class Prueba001Pral {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Esto es una Prueba la versión es 1.0");
+        
+    }
+    
+}
